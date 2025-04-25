9f507ed42599b74e19a39f58b1846d40b1339b9d
Pulkit0111 <ptyagi1551@gmail.com>
2025-04-25 21:25:56 +0530

    added the capability to genrate content directly from terminal

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import os
 from dotenv import load_dotenv
 from agents import research_agent, writer_agent, editor_agent
-
+import argparse
 load_dotenv()
 
+def save_to_markdown(content, topic):
+    filename = f"{topic.lower().replace(' ', '_')}.md"
+    with open(filename, 'w', encoding='utf-8') as file:
+        file.write(content)
+    print(f"\n📝 Article saved to {filename}")
+    
 def run_pipeline(topic):
     print(f"\n📌 Running Multi-agent content pipeline for: {topic}\n")
     #step 1: research
@@ -12,11 +18,16 @@ def run_pipeline(topic):
     draft_article = writer_agent(research_summary)
     #step 3: edit
     polished_article = editor_agent(draft_article)
-    
-    return polished_article
-    
+    save_to_markdown(polished_article, topic)
+
 if __name__ == "__main__":
-    topic = input("Enter a topic to generate the article: ")
-    article = run_pipeline(topic)
-    print("\n📝 Final Article:")
-    print(article)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--topic", help="Topic to generate article for")
+    args = parser.parse_args()
+
+    if args.topic:
+        run_pipeline(args.topic)
+    else:
+        topic = input("Enter a topic to generate content: ")
+        run_pipeline(topic)
+
